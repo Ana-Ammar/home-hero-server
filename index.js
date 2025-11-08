20ddaeb6dce3682ba641666d88f9bb6a0541bfdc
Ana-Ammar <sanimammar818@gmail.com>
2025-11-08 22:12:59 +0600

    added service get, post and delete api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5165;
 
@@ -31,10 +31,36 @@ const run = async () => {
     const serviceCollection = db.collection("services");
     const bookingCollection = db.collection("bookings");
 
+    // Services API
 
-    
+    app.get("/services", async (req, res) => {
+      const { email, category } = req.query;
+      const query = {};
+      if (email) query.email = email;
+      if (category) query.category = category;
+      const services = await serviceCollection.find(query).toArray();
+      res.send(services);
+    });
+
+    app.post("/services", async (req, res) => {
+      const newService = await serviceCollection.insertOne(req.body);
+      res.send(newService);
+    });
+
+    app.delete("/services/:id", async (req, res) => {
+      const query = { _id: new ObjectId(req.params.id) };
+      const deleteService = await serviceCollection.deleteOne(query);
+      res.send(deleteService);
+    });
+
+
+
+    // Booking API
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
   }
 };
